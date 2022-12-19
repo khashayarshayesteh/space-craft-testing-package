08aa8c849717012571c9b345e0143a32e82db858
khashayar shayesteh <shayesteh.office@gmail.com>
2022-12-19 08:52:51 -0800

    space-create-testing

---
+++ src/test/java/CreatePlayerServiceImplTest.java
@@ -0,0 +1,36 @@
+import com.cydeo.spacecraft.dto.CreateGameDTO;
+import com.cydeo.spacecraft.entity.Player;
+import com.cydeo.spacecraft.enumtype.Boost;
+import com.cydeo.spacecraft.enumtype.Level;
+import com.cydeo.spacecraft.service.impl.CreatePlayerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreatePlayerServiceImplTest {
+
+    private CreatePlayerServiceImpl createPlayerService;
+
+    @BeforeEach
+    public void setUp(){
+        createPlayerService = new CreatePlayerServiceImpl();
+    }
+
+    @Test
+    public void should_create_player_with_big_bomb_boost_type_and_level_easy(){
+        // given
+        CreateGameDTO createGameDTO = new CreateGameDTO();
+        createGameDTO.setBoost(Boost.BIG_BOMB);
+        createGameDTO.setLevel(Level.EASY);
+        createGameDTO.setUsername("username");
+
+        //when
+        Player player = createPlayerService.createPlayer(createGameDTO);
+
+        //then
+        assertEquals(player.getHealth(), 2000);
+        assertEquals(player.getArmor(), 7);
+        assertEquals(player.getShootPower(), 5010);
+    }
+}
